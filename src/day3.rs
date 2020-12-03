b23f1e3d0094b694e9bdae143fdd230c70a1811b
Daniel Prilik <danielprilik@gmail.com>
2020-12-03 11:00:52 -0500

    day3 cleanup

--- src/day3.rs
+++ src/day3.rs
@@ -3,46 +3,30 @@ use crate::prelude::*;
 macro_rules! munge_input {
     ($input:ident) => {{
         let input = $input;
-        input
-            .split('\n')
-            .map(|ln| ln.chars().collect::<Vec<char>>())
-            .collect::<Vec<_>>()
+        input.split('\n').map(|ln| ln.as_bytes())
     }};
+}
+
+fn count_trees<'a>(lns: impl Iterator<Item = &'a [u8]>, (dx, dy): (usize, usize)) -> usize {
+    lns.step_by(dy)
+        .zip((0..).step_by(dx))
+        .filter(|(ln, x)| ln[x % ln.len()] == b'#')
+        .count()
 }
 
 pub fn q1(input: &str, _args: &[&str]) -> DynResult<usize> {
     let input = munge_input!(input);
-
-    let mut trees = 0;
-    let mut x = 0;
-    for (_y, ln) in input.into_iter().enumerate() {
-        if ln[x % ln.len()] == '#' {
-            trees += 1;
-        }
-        x += 3;
-    }
-
-    Ok(trees)
+    let total = count_trees(input, (3, 1));
+    Ok(total)
 }
 
 pub fn q2(input: &str, _args: &[&str]) -> DynResult<usize> {
     let input = munge_input!(input);
-
-    const SLOPES: &[(usize, usize)] = &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-
-    let mut total = 1;
-    for &(dx, dy) in SLOPES {
-        let mut trees = 0;
-        let mut x = 0;
-        for ln in input.iter().step_by(dy) {
-            if ln[x % ln.len()] == '#' {
-                trees += 1;
-            }
-            x += dx;
-        }
-
-        total *= trees;
-    }
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let total = slopes
+        .iter()
+        .map(|slope| count_trees(input.clone(), *slope))
+        .product();
 
     Ok(total)
 }
